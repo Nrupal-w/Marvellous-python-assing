89c189466889c15f170db95a7dcb517a3d175235
Nrupal-w <nrupalw@gmail.com>
2025-06-02 14:01:23 +0530

    Update Assigment1_8.py

--- Assigment 1/Assigment1_8.py
+++ Assigment 1/Assigment1_8.py
@@ -1,3 +1,4 @@
 a=int(input("Enter a number"))
 for i in range(0,a):
     print("*",end=" ")
+#print * n number of times as the user mentioned using for function and end ="" so then the curser dont move to the next line
