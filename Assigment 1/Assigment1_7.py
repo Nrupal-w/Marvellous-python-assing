6c1d27575403ef4f7773f860beb5906aaada6182
Nrupal-w <nrupalw@gmail.com>
2025-06-02 14:00:18 +0530

    Update Assigment1_7.py

--- Assigment 1/Assigment1_7.py
+++ Assigment 1/Assigment1_7.py
@@ -6,3 +6,4 @@ def fun(a):
 a=int(input("Enter a number "))
 val=fun(a)
 print(val)
+#created a function named fun to see if the number is divisible by 5 using if statemt with modulus if the remainder is 0 the it is divisible form 0 else not and then it is layter called in the code to take the user input and chek 
