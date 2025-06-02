db69db1f4e6b7dfe5d7318f4827e1aa87b5a5d50
Nrupal-w <nrupalw@gmail.com>
2025-06-02 13:49:59 +0530

    Update Assigment1_3.py

--- Assigment 1/Assigment1_3.py
+++ Assigment 1/Assigment1_3.py
@@ -6,3 +6,5 @@ a=int(input("Enter a number"))
 b=int(input("Enter a number"))
 sum=Add(a,b)
 print("Sum of given two number is",sum)
+
+#created a fuction to accept 2 numbers and then add the 2 numbers and save the sum in variable and then return the variable to the main when later callend in the code the add function adds 2 number and returns the sum of the user given input
