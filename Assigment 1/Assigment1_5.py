0c4a360b2da07d5dc012644179bea33689b1f56f
Nrupal-w <nrupalw@gmail.com>
2025-06-02 13:56:35 +0530

    Update Assigment1_5.py

--- Assigment 1/Assigment1_5.py
+++ Assigment 1/Assigment1_5.py
@@ -2,3 +2,4 @@ i=10
 while i>=0:
     print(i,end=" ")
     i-=1
+#used while loop and assinged i as 10 then run the loop form 10 to 0 printing all number from 10 to 1 in oder and with each itreation reducing the value of 1 so all numbers are printed 
