42e2329ede3da549890d9ef81bda702681c11a1a
Nrupal-w <nrupalw@gmail.com>
2025-06-02 13:57:39 +0530

    Update Assigment1_6.py

--- Assigment 1/Assigment1_6.py
+++ Assigment 1/Assigment1_6.py
@@ -5,3 +5,4 @@ elif a<0:
     print("Negative Number")
 else:
     print("Zero")
+# taking input form user and using if elif and else satment to chek if number is gerater then 0 or les then 0 or zero
